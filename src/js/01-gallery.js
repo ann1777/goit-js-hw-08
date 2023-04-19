3a4fa3b420c6a6cdabfaaf0bc52a1125c76acb1f
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-19 17:08:43 +0300

    Moved project files to the src folder of the parcel-project-template. Installed dependensies to the package.json.

---
+++ src/js/01-gallery.js
@@ -0,0 +1,69 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+//1.Creating markup based on the galleryItems data array and the provided gallery item template.
+ 
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClickCreateModal);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+  });
+
+function onImgClickCreateModal(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {return;}
+
+    const isItemImage = e.target.classList.contains("gallery__image");
+    if (!isItemImage) {
+        return;
+    }
+
+    const currentImgUrl = e.target.dataset.source;
+
+    const instance = basicLightbox.create(
+        `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+        {
+            onShow: (instance) => {
+                window.addEventListener("keydown", onEscKeyPress);
+            },
+            onClose: (instance) => {
+                window.removeEventListener("keydown", onEscKeyPress);
+            },
+        }
+    );
+    instance.show();
+
+    function onEscKeyPress(event) {
+        const ESC_KEY_CODE = 'Escape';
+        const isEscKey = event.code === ESC_KEY_CODE;
+        if (isEscKey) {
+            instance.close();
+        }
+    }
+}
+
+console.log(galleryItems);
---
+++ src/js/02-video.js
@@ -0,0 +1,37 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+
+
+const getCurrentTime = function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+  };
+
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0);
+
+player
+  .setColor('#d8e0ff')
+  .then(function (color) {
+    console.log('The new color value: #D8E0FF');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
